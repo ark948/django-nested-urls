bcd1f9833533dbdaac65e226486dcf82b4d12d47
ark948 <armankarimi948@gmail.com>
2024-06-02 15:19:36 +0330

    Done

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,4 +4,5 @@ from .views import index
 app_name = 'blog'
 urlpatterns = [
     path("", index, name="index"),
+    path("api/", include("apps.blog.api.urls")),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 # Create your views here.
 
+@api_view(['GET'])
 def index(request):
-    return render(request, "blog/index.html")
+    return Response(
+        {
+            "api link": reverse("blog:api:index", request=request),
+            "front link": "none"
+        }
+    )
